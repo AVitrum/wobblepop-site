37b27f4abd4a34720bb3e7d42d4ba2402d7470d3
AVitrum <andrey.almashi@gmail.com>
2025-08-05 09:39:05 +0300

    feat: updated redirection

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
 );
